faf28e7e0ba41905e4f2a20e86fca673fa4aca53
IgorMihailov1303 <alfred.stalkertop@gmail.com>
2024-09-16 15:58:29 +0300

    lesson conspect 16.09.2024

---
+++ conspect/perevod v raznie sistemi.py
@@ -0,0 +1,15 @@
+#while True:
+#    num = int(input())
+#    base = 5
+#    new = ''
+#    while num > 0:
+#        new = str(num%base) + new
+#        num = num // base
+#    print(new)
+
+letters = '0123456789ABCDEFGH'
+
+a = 'AB9CH'
+
+
+print(int(a, 20))
---
+++ conspect/rabota_s_failom.py
@@ -0,0 +1,28 @@
+f = open('1.txt') #открыть файл
+m = f.readlines() #засовывает все строчки файла в массив
+
+with open('1.txt') as f: #нормальный способ чтения фалов без забивания памяти
+    m = f.readline()     #
+    print(m)             #
+    for s in f:          #
+        s = s.strip()    #
+        print(s)         #
+
+k = 12
+s = f"aaaaaaaaaaaaaaaa{k}ssss" #позволяет соединять переменные и строки
+print(s)
+
+s = f'{k}' #делает из переменной строку
+
+s = 'asdjfgeopipoajfj'
+s = s.replace('a', 'XX', 1) #поменять "а" на "ХХ" N раз (отсчёт c лева на право)
+print(s)
+
+
+s = '9' * 127
+while ('333' in s) or ('999' in s):
+    if '333' in s:
+        s = s.replace('333', '9', 1)
+    else:
+        s = s.replace('999', '3', 1)
+print(s)
